2b26c5d3f1c6dcd57f9617c64504d3ba4d435659
tnilabydnas <33456820+tnilabydnas@users.noreply.github.com>
2018-03-19 18:15:03 +0200

    Add files via upload
    Because of the merging problems(I think) I can't commit the files via IntelliJ normally. Files contain a couple more filters and reports, documentations and tests for them.

---
+++ AssignmentController.java
@@ -0,0 +1,163 @@
+package ro.ubb.labproblems.controller;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import ro.ubb.labproblems.domain.entities.Assignment;
+import ro.ubb.labproblems.domain.validators.ValidatorException;
+import ro.ubb.labproblems.repository.Repository;
+import ro.ubb.labproblems.utils.IteratorUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Implements business logic for {@link Assignment assignments}.
+ * Functions returns a printable version of solution to the ui.
+ */
+public class AssignmentController {
+
+    /**
+     * Storage for the assignments
+     */
+    private final Repository<String, Assignment> assignmentRepository;
+
+    /**
+     * Constructor for the controller
+     *
+     * @param assignmentRepository {@link Repository} to use for storage
+     */
+    public AssignmentController(Repository<String, Assignment> assignmentRepository) {
+        this.assignmentRepository = assignmentRepository;
+    }
+
+
+    /**
+     * Assigns a given problem to a given student
+     * @param problemTitle The title of the problem
+     * @param studentRegistrationNumber The registration number of the student
+     * @return A message telling the assignment was successful, or the reason why it wasn't
+     */
+    public String assign(String problemTitle, String studentRegistrationNumber) {
+        try {
+            return assignmentRepository.save(new Assignment(problemTitle, studentRegistrationNumber))
+                    .map(student -> "Problem was already assigned to student")
+                    .orElse("Assigned successfully");
+        } catch (ValidatorException e) {
+            return e.getMessage();
+        }
+    }
+
+    /**
+     * Assigns a grade to a given assignment
+     * @param problemTitle The title of the problem
+     * @param studentRegistrationNumber The registration number of the student
+     * @param grade The given grade
+     * @return A message telling the assignment was successful, or the reason why it wasn't
+     */
+    public String grade(String problemTitle, String studentRegistrationNumber, Double grade) {
+        Assignment assignment = new Assignment(problemTitle, studentRegistrationNumber);
+        assignment.setGrade(grade);
+        try {
+            return assignmentRepository.update(assignment)
+                    .map(student -> "Problem was not assigned to student")
+                    .orElse("Graded successfully");
+        } catch (ValidatorException e) {
+            return e.getMessage();
+        }
+    }
+
+    /**
+     * Unassigns a problem from a student
+     * @param problemTitle The title of the problem
+     * @param studentRegistrationNumber The registration number of the student
+     * @return A message telling the unassignment was successful, or the reason why it wasn't
+     */
+    public String unassign(String problemTitle, String studentRegistrationNumber) {
+        return assignmentRepository.delete(new Assignment(problemTitle, studentRegistrationNumber).getIdentifier())
+                .map(assignment -> "Unassigned successfully")
+                .orElse("No assignment was present");
+    }
+
+    /**
+     * Returns all assignments as a list
+     * @return
+     */
+    public List<Assignment> getAllAssignments() {
+        return IteratorUtils.toList(assignmentRepository.findAll());
+    }
+
+    /**
+     * Finds the problem that was assigned most times
+     * @return The title of the most assigned problem
+     */
+    public String mostAssignedProblem() {
+        List<Assignment> assignments = getAllAssignments();
+        Map<String, Integer> freqs = new HashMap<>();
+        for (Assignment a : assignments) {
+            freqs.merge(a.getProblemTitle(),
+                    1,
+                    Integer::sum);
+        }
+        Integer count = freqs.values().stream().max(Comparator.comparingInt(x -> (int) x)).get();
+        return freqs.entrySet().stream().filter(x -> x.getValue().equals(count)).findFirst().get().getKey();
+    }
+
+    /**
+     * Returns those assignments that are assigned to a given student
+     * @param registrationNumber The registration number of the student
+     * @return A list of filtered assignments
+     */
+    public List<Assignment> filterByStudent(String registrationNumber) {
+        return getAllAssignments()
+                .stream()
+                .filter(assignment -> (assignment.getStudentRegistrationNumber().equals(registrationNumber)))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns those assignments that contain a given problem, and the grade given for it is greater or equal to 8
+     * @param problemTitle The title of the problem
+     * @return A list of filtered assignments
+     */
+    public List<Assignment> filterByGrade(String problemTitle) {
+        return getAllAssignments()
+                .stream()
+                .filter(assignment -> (assignment.getProblemTitle().equals(problemTitle) && assignment.getGrade()>=8))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @return String representation of all {@link Assignment assignments} from the {@link Repository repository}
+     */
+    public String showAll() {
+        return IteratorUtils
+                .stream(assignmentRepository.findAll())
+                .map(Object::toString)
+                .collect(Collectors.joining("\n"));
+    }
+
+    /**
+     * Finds the best student based on the average of his/her grades
+     * @return The identification number of the student
+     */
+    public String bestStudent() {
+        List<Assignment> assignments=getAllAssignments();
+        Map<String, Integer> freqs = new HashMap<>();
+        for (Assignment a : assignments) {
+            freqs.merge(a.getStudentRegistrationNumber(),
+                    1,
+                    Integer::sum);
+        }
+
+        Map<String, Double> grades = new HashMap<>();
+        for (Assignment a : assignments) {grades
+                .merge(a.getStudentRegistrationNumber()
+                    ,a.getGrade(),
+                    Double::sum);
+        }
+
+        grades.forEach((k,v) -> v=v/(freqs.get(k)));
+        Double maxvalue = grades.values().stream().max(Comparator.comparingDouble(x-> (Double) x)).get();
+        return grades.entrySet().stream().filter(x -> x.getValue().equals(maxvalue)).findFirst().get().getKey();
+
+    }
+}
---
+++ StudentsMenu.java
@@ -0,0 +1,95 @@
+package ro.ubb.labproblems.ui;
+
+import ro.ubb.labproblems.controller.StudentController;
+
+import java.util.Scanner;
+
+/**
+ * Extending {@link CommandMenu CommandMenu}, the StudentsMenu defines the 4 keywords in the map(add, all, remove, update), and then works with the repository's student-type entities
+ */
+public class StudentsMenu extends CommandMenu {
+
+    /**
+     * {@link StudentController StudentController} to support the StudentMenu
+     */
+    private final StudentController studentController;
+
+    /**
+     * Constructor of StudentsMenu
+     *
+     * @param studentController {@link StudentController StudentController}-type object
+     * @param scanner           {@link Scanner Scanner}
+     */
+    public StudentsMenu(StudentController studentController, Scanner scanner) {
+        super("Students operations", scanner);
+        this.studentController = studentController;
+        registerCommands();
+    }
+
+    @Override
+    protected void registerCommands() {
+        registerCommand("add", "Adds a new student", this::addCommand);
+        registerCommand("all", "Shows all students", this::showAllCommand);
+        registerCommand("remove", "Remove a student", this::removeCommand);
+        registerCommand("update", "Update a student", this::updateCommand);
+        registerCommand("group","All students of a group", this::groupFilter);
+        super.registerCommands();
+    }
+
+    /**
+     * Command for showing all the students in the repository
+     */
+    private void showAllCommand() {
+        System.out.println(studentController.showAll());
+    }
+
+    /**
+     * Command for adding a new student into the repository
+     */
+    private void addCommand() {
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Registration number: ");
+        String registrationNumber = scanner.nextLine();
+
+        System.out.print("Group number: ");
+        Integer groupNumber = scanner.nextInt();
+
+        System.out.println(studentController.add(name, registrationNumber, groupNumber));
+    }
+
+    /**
+     * Command for removing a student from the repository
+     */
+    private void removeCommand() {
+        System.out.print("Registration number: ");
+        String registrationNumber = scanner.nextLine();
+
+        System.out.println(studentController.remove(registrationNumber));
+
+    }
+
+    /**
+     * Command for updating a student in the repository
+     */
+    private void updateCommand() {
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Registration number: ");
+        String registrationNumber = scanner.nextLine();
+
+        System.out.print("Group number: ");
+        Integer groupNumber = scanner.nextInt();
+
+        System.out.println(studentController.update(name, registrationNumber, groupNumber));
+    }
+
+    private void groupFilter() {
+        System.out.print("Group number: ");
+        Integer groupNumber = scanner.nextInt();
+
+        System.out.println(studentController.filterByGroup(groupNumber));
+    }
+}
