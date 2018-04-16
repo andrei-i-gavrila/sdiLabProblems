89da5865f3d0ad0751b0458d9c58743ce6781480
tnilabydnas <tnilabydnas@yahoo.com>
2018-04-16 22:46:22 +0300

    Todos filled (not sure if it's correct or not, I still couldn't compile the new project)

--- labAppsProblems-client/src/main/java/ro/ubb/labproblems/controller/AssignmentRestController.java
+++ labAppsProblems-client/src/main/java/ro/ubb/labproblems/controller/AssignmentRestController.java
@@ -1,22 +1,48 @@
 package ro.ubb.labproblems.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class AssignmentRestController {
+
+    private RestTemplate restTemplate;
+    private String baseUrl;
+
+    public AssignmentRestController(RestTemplateBuilder restTemplateBuilder, @Value("${server.baseUrl}") String baseUrl) {
+        this.restTemplate = restTemplateBuilder.build();
+        this.baseUrl = baseUrl;
+    }
+
     public String showAll() {
-        return "TODO";
+        return restTemplate.getForObject(baseUrl + "/assignments", String.class);
     }
 
-    public String unassign(String problemTitle, String studentRegistrationNumber) {
-        return "TODO";
+    public String unassign(String registrationNumber, String title) {
+        return restTemplate.postForObject(baseUrl + "assigmnetss/{registrationNumber}/delete", null, String.class, title);
     }
 
-    public String grade(String problemTitle, String studentRegistrationNumber, Double grade) {
-        return "TODO";
+    public String grade(String registrationNumber, String title, Double grade) {
+        String uri = UriComponentsBuilder
+                .fromUriString(baseUrl + "/assignments/{registrationNumber}")
+                .queryParam("registrationNumber", registrationNumber)
+                .queryParam("title", title)
+                .queryParam("grade",grade)
+                .build(title).toString();
+
+        return restTemplate.postForObject(uri, null, String.class);
     }
 
-    public String assign(String problemTitle, String studentRegistrationNumber) {
-        return "TODO";
+    public String assign(String registrationNumber, String problemTitle) {
+        String uri = UriComponentsBuilder
+                .fromUriString(baseUrl + "/assignments/create")
+                .queryParam("registrationNumber", registrationNumber)
+                .queryParam("problemTitle", problemTitle)
+                .build().toUriString();
+
+        return restTemplate.postForObject(uri, null, String.class);
     }
 }
--- labAppsProblems-client/src/main/java/ro/ubb/labproblems/controller/ProblemRestController.java
+++ labAppsProblems-client/src/main/java/ro/ubb/labproblems/controller/ProblemRestController.java
@@ -1,24 +1,48 @@
 package ro.ubb.labproblems.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class ProblemRestController {
+
+    private RestTemplate restTemplate;
+    private String baseUrl;
+
+    public ProblemRestController(RestTemplateBuilder restTemplateBuilder, @Value("${server.baseUrl}") String baseUrl) {
+        this.restTemplate = restTemplateBuilder.build();
+        this.baseUrl = baseUrl;
+    }
+
     public String showAll() {
-        return "Todo";
+        return restTemplate.getForObject(baseUrl + "/problems", String.class);
     }
 
     public String add(String title, String description) {
-        return "Todo";
+        String uri = UriComponentsBuilder
+                .fromUriString(baseUrl + "/problems/create")
+                .queryParam("title",title)
+                .queryParam("description", description)
+                .build().toUriString();
 
+
+        return restTemplate.postForObject(uri, null, String.class);
     }
 
     public String remove(String title) {
-        return "Todo";
-
+        return restTemplate.postForObject(baseUrl + "problems/{title}/delete", null, String.class, title);
     }
 
-    public String update(String title, String description) {
-        return "Todo";
+    public String update(String title, String descripton) {
+        String uri = UriComponentsBuilder
+                .fromUriString(baseUrl + "/problems/{title}")
+                .queryParam("title", title)
+                .queryParam("description", descripton)
+                .build(title).toString();
+
+        return restTemplate.postForObject(uri, null, String.class);
     }
 }
